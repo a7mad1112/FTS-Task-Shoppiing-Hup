a5eda36c2d63bc0160f82dc333c0dcd5a2ddbc10
a7mad1112 <ahmalawneh79@gmail.com>
2023-05-02 22:21:32 +0200

    features section in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,31 @@ import { Link } from "react-router-dom";
 import heroImg from "../assets/images/hero2.svg";
 import "./pagesStyle/home.css";
 import Categories from "../component/UI/cagegories/Categories";
+
+// data for features section
+import featureImg01 from "../assets/images/fastShipping.svg";
+import featureImg02 from "../assets/images/santa.svg";
+import featureImg03 from "../assets/images/week.svg";
+const featureData = [
+  {
+    title: "Fast Shipping",
+    imgUrl: featureImg01,
+    desc: "Get your order quickly and efficiently with our fast shipping options.",
+    key: 1,
+  },
+  {
+    title: "Hassle-Free Returns",
+    imgUrl: featureImg02,
+    desc: "Not satisfied with your purchase? No problem. We offer hassle-free returns.",
+    key: 2,
+  },
+  {
+    title: "24/7 Customer Support",
+    imgUrl: featureImg03,
+    desc: "Have a question or concern? Our customer support team is available 24/7 to assist you.",
+    key: 3,
+  },
+];
 const Home = () => {
   return (
     <Helmet title="Home">
@@ -59,8 +84,45 @@ const Home = () => {
           </Row>
         </Container>
       </section>
+      {/* === Categories Section === */}
       <section>
         <Categories />
+      </section>
+      {/* === Features Section === */}
+      <section>
+        <Container>
+          <Row>
+            <Col className="text-center" lg="12">
+              <h5 className="feature_subtitle mb-4">What we offer</h5>
+              <h2 className="feature_title">Sit back and relax</h2>
+              <h2 className="feature_title">
+                we'll <span>deliver</span>
+              </h2>
+              <p className="mb-1 mt-4 feature_text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Perferendis, placeat.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Accusamus, ipsam!
+              </p>
+            </Col>
+
+            {featureData.map((item) => (
+              <Col className="mt-5" lg="4" md="6" sm="6" key={item.key}>
+                <div className="feature_item text-center px-5 py-3">
+                  <img
+                    className="mb-3"
+                    src={item.imgUrl}
+                    alt="feature-img"
+                  />
+                  <h5 className="fw-bold">{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </section>
     </Helmet>
   );
