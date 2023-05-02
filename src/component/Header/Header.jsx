e73c99b8468cd10bba5783a2d92445c765912b37
a7mad1112 <ahmalawneh79@gmail.com>
2023-05-02 13:44:08 +0200

    complete header

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { Container } from "reactstrap";
+import { Link, NavLink } from "react-router-dom";
+import "./header.css";
+
+const NAV_LINKS = [
+  {
+    toDisplay: "Home",
+    path: "/",
+    key: 0,
+  },
+  {
+    toDisplay: "Products",
+    path: "/products",
+    key: 1,
+  },
+  {
+    toDisplay: "Cart",
+    path: "/cart",
+    key: 2,
+  },
+  {
+    toDisplay: "Checkout",
+    path: "/checkout",
+    key: 3,
+  },
+];
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const menuRef = useRef(null);
+  const headerRef = useRef(null);
+  const toggleMenu = () => menuRef.current.classList.toggle("show_menu");
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      )
+        headerRef.current.classList.add("header_shrink");
+      else headerRef.current.classList.remove("header_shrink");
 
-export default Header
+      return () => window.removeEventListener("scroll");
+    });
+  }, []);
+
+  return (
+    <>
+      <header ref={headerRef} className="header">
+        <Container>
+          <div className="nav_wrapper d-flex align-items-center justify-content-between">
+            <div className="logo">
+              <h1>
+                <span className="text-blue">Shopping</span>
+                <span className="text-gold">Hub.</span>
+              </h1>
+            </div>
+
+            {/* === menu === */}
+            <div className="navigation" ref={menuRef} onClick={toggleMenu}>
+              <nav className="menu d-flex align-items-center gap-5">
+                {NAV_LINKS.map((item) => (
+                  <NavLink
+                    to={item.path}
+                    key={item.key}
+                    className={(navClass) =>
+                      navClass.isActive ? "active_menu" : null
+                    }
+                  >
+                    {item.toDisplay}
+                  </NavLink>
+                ))}
+              </nav>
+            </div>
+
+            {/* === right icons === */}
+            <div className="nav_right d-flex align-items-center gap-4">
+              <span className="cart_icon">
+                <i className="ri-shopping-basket-line"></i>
+                <span className="cart_badge">{"totalQuantity" && 4}</span>
+              </span>
+              <span className="user">
+                <Link to="/login">
+                  <i className="ri-user-line"></i>
+                </Link>
+              </span>
+              <span className="mobile_menu" onClick={toggleMenu}>
+                <i className="ri-menu-line"></i>
+              </span>
+            </div>
+          </div>
+        </Container>
+      </header>
+      <div></div>
+    </>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Routers } from "react-router-dom";
+import 'remixicon/fonts/remixicon.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
