2676c9d960ee85737e76ed0c9174c4bf186dd068
a7mad1112 <ahmalawneh79@gmail.com>
2023-05-05 01:01:02 +0200

    login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
-
+import React from "react";
+import Helmet from "./../component/Helmet/Helmet";
+import CommonSection from "./../component/UI/commom-section/CommonSection";
+import { Col, Container, Row } from "reactstrap";
+import { Link } from "react-router-dom";
+import "./pagesStyle/login.css";
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Helmet title="Login">
+      <CommonSection title="Login" />
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12" className="m-auto text-center">
+              <form className="form mb-5" onSubmit={(e) => e.preventDefault()}>
+                <div className="form_group">
+                  <input type="email" required placeholder="Email" />
+                </div>
+                <div className="form_group">
+                  <input type="password" required placeholder="Password" />
+                </div>
+                <button className="addToCart_btn" type="submit">
+                  Login
+                </button>
+              </form>
+              <Link to="/register">
+                Don't have an account? Create an account
+              </Link>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
 
-export default Login
+export default Login;
